b14326a78be211468f53b15126d642a38913d23d
Akhil353 <asingamneni@gmail.com>
2024-10-19 16:01:42 -0700

    added basic table code for student datatable

--- src/main/java/com/nighthawk/spring_portfolio/mvc/student/Student.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/student/Student.java
@@ -1,6 +1,8 @@
 package com.nighthawk.spring_portfolio.mvc.student;
 
 
+import java.util.ArrayList;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,15 +23,47 @@ public class Student {
     private Long id;
 
     private String name;
-    private String githubUsername;
+    private String username;
     private int table;
     private String course;
 
-    public Student(String name, String githubUsername, int table, String course) {
+    public Student(String name, String username, int table, String course) {
         this.name = name;
-        this.githubUsername = githubUsername;
+        this.username = username;
         this.table = table;
         this.course = course;
     }
+
+    public static Student createStudent(String name, String username, int table, String course) {
+        Student student = new Student();
+        student.setName(name);
+        student.setUsername(username);
+        student.setTable(table);
+        student.setCourse(course);
+
+        return student;
+    }
     
+        public static Student[] init() {
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(createStudent("Akhil Singamneni", "Akhil353", 4, "CSA"));
+        students.add(createStudent("Srinivas Nampalli", "srininampalli", 4, "CSA"));
+        students.add(createStudent("Aditya Samavedam", "adityasamavedam", 4, "CSA"));
+        students.add(createStudent("Nitin Balaji", "nitinsandiego", 4, "CSA"));
+        return students.toArray(new Student[0]);
+    }
+
+    /** Static method to print Person objects from an array
+     * @param args, not used
+     */
+    public static void main(String[] args) {
+        // obtain Person from initializer
+        Student students[] = init();
+
+        // iterate using "enhanced for loop"
+        for( Student student : students) {
+            System.out.println(student);  // print object
+        }
+    }
+
 }
