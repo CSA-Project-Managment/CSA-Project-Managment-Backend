f3196a6f96e9c473c87ac6985d1daa70a72e25d0
nitinsandiego <nitinsandiego@gmail.com>
2024-10-21 12:03:33 -0700

    Update StudentApiController.java

--- src/main/java/com/nighthawk/spring_portfolio/mvc/student/StudentApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/student/StudentApiController.java
@@ -1,27 +1,21 @@
 package com.nighthawk.spring_portfolio.mvc.student;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/students")
 public class StudentApiController {
+    
     @Autowired
-    private StudentJPARepository repository;
+    private Student.StudentService studentService;
 
-    @GetMapping
-    public List<Student> getAllStudents() {
-        return repository.findAll();
+    @GetMapping("/all")
+    public ResponseEntity<Iterable<Student>> getAllFlights() {
+        return ResponseEntity.ok(studentService.findAll());
     }
 
-    @PostMapping
-    public Student createStudent(@RequestBody Student student) {
-        return repository.save(student);
-    }
 }
