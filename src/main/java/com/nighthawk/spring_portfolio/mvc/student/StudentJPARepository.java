0208d2203f26e0d37faece924cbe948c7d2a93e9
nitinsandiego <nitinsandiego@gmail.com>
2024-10-21 14:12:47 -0700

    Update StudentJPARepository.java

--- src/main/java/com/nighthawk/spring_portfolio/mvc/student/StudentJPARepository.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/student/StudentJPARepository.java
@@ -1,8 +1,11 @@
 package com.nighthawk.spring_portfolio.mvc.student;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 // JPA is an object-relational mapping (ORM) to persistent data, originally relational databases (SQL). Today JPA implementations has been extended for NoSQL.
 public interface StudentJPARepository extends JpaRepository<Student, Long> {
-    // Connection
+    Optional<Student> findByUsername(String username);
+    Optional<Student> findByName(String name);
 }
